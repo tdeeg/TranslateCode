55f6cbade5cdc87ef71bf9a24bb4dc67534a27ef
tdeeg <150019070+tdeeg@users.noreply.github.com>
2023-11-09 11:23:22 +0100

    Programm File

---
+++ Übersetzter ChatGPT/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+using System.IO;
+
+namespace ÜbersetzerChatGPT
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Füge deinen API Key hinzu 
+            OpenAIAPI api = 
+                new OpenAIAPI("");
+
+            //Vorstellung Programm mit Funktionsweise
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(File.ReadAllText("D:\\Schule\\PGC\\vs_projects (1)\\vs_projects\\Übersetzter ChatGPT\\Beschreibung.txt"));
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Drücken sie Enter");
+            Console.ResetColor();
+            Console.ReadLine();
+            Console.Clear();
+           
+            //Einlesen von Quellcode mit Auswahl von welcher Sprache in welche andere übersetzt werden soll
+
+            Console.WriteLine("Herzlich Willkommen, bitte wählen Sie die Sprache aus der übersetzt werden soll:");
+            string input_language = Console.ReadLine();
+            Console.WriteLine("Bitte geben Sie nun den Code ein den Sie übersetzt haben möchten:");
+            string code = Console.ReadLine();
+            Console.WriteLine("Wählen Sie jetzt noch die Ziel-Sprache in die Sie den Code übersetzt haben möchten:");
+            string output_language = Console.ReadLine();    
+            ChatMessage msg = new ChatMessage();
+            msg.Content = "Übersetzte folgenden" + input_language + "Code:" + code + "in" + output_language;
+
+            
+            
+
+            ChatMessage[] messages = new ChatMessage[1];
+            messages[0] = msg;
+            
+
+
+            ChatRequest req = new ChatRequest();
+            req.Model = Model.ChatGPTTurbo;
+            req.Temperature = 0.1;
+            req.MaxTokens = 1000;
+            req.Messages = messages;
+
+            Task<ChatResult> result = api.Chat.CreateChatCompletionAsync(req);
+            result.Wait();
+
+            Console.WriteLine(result.Result);
+
+           
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
